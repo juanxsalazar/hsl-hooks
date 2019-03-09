c7e26a8c858b3e6fef564c9888b712febdee1b9b
Juan <juanxsalazar21@gmail.com>
2019-03-09 00:11:42 -0500

    hsl input is working and changes background color

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export default function HelloWorld () {
-    return <>
+export default function HelloWorld (props) {
+  const [hue, changeHue] = useState(0)
+  const [saturation, changeSaturation] = useState(0)
+  const [lightness, changeLightness] = useState(0)
+   
+   return <>
     <header>
     <h1>React Hooks HSL Color Changer!</h1>
     <section>
-      <section className="square"></section>
+      <section className="square"
+        style={{
+          backgroundColor: `hsl(${hue},${saturation}%,${lightness}%)`
+        }}
+      ></section>
       <section className="controls">
       <h3>Hue</h3>
-      <input type="range"/>
+      <input type="range" value={hue} max="360" 
+      onChange={event => changeHue(event.target.value)}/>
       <h3>Saturation</h3>
-      <input type="range"/>
+      <input type="range" value={saturation}
+      onChange={event => changeSaturation(event.target.value)}/>
       <h3>Lightness</h3>
-      <input type="range"/>
+      <input type="range" value={lightness}
+      onChange={event => changeLightness(event.target.value)}/>
       </section>
     </section>
     <p>HSL: () </p>
