d42931f354dde6a9f59256ece914fd7dbc7b7602
Juan <juanxsalazar21@gmail.com>
2019-03-08 23:51:33 -0500

    html added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
-function App () {
+export default function App () {
     return <HelloWorld />
 }
 
-export default App
+ 
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
 
-class HelloWorld extends Component {
-  render() {
-    return <h1>Color, World!</h1>
-  }
+export default function HelloWorld () {
+    return <>
+    <header>
+    <h1>React Hooks HSL Color Changer!</h1>
+    <section>
+      <section className="square"></section>
+      <section className="controls">
+      <h3>Hue</h3>
+      <input type="range"/>
+      <h3>Saturation</h3>
+      <input type="range"/>
+      <h3>Lightness</h3>
+      <input type="range"/>
+      </section>
+    </section>
+    <p>HSL: () </p>
+    </header>
+    <section className="middle">
+    <button className="save-color">Remember Color</button>
+    <button>Random Color</button>
+    </section>
+    <section className="saved-list">
+    <h2>Saved Colors:</h2>
+    <ul>
+      <li className="saved-color">
+      <section className="square">
+      </section>
+      <p className="small-font">HSL: () </p>
+      </li>
+    </ul>
+    </section>
+     </>
 }
 
-export default HelloWorld
+
